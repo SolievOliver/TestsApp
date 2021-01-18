0d8cd2425a41ecffe2c13074222a0190d12c20a9
SolievOliver <68802261+SolievOliver@users.noreply.github.com>
2021-01-18 15:18:18 +0500

    Made clear

--- app/src/main/java/com/oliverworks/testsapp/MainActivity.kt
+++ app/src/main/java/com/oliverworks/testsapp/MainActivity.kt
@@ -2,28 +2,43 @@ package com.oliverworks.testsapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.oliverworks.testsapp.ui.dashboard.SavedFragment
+import com.oliverworks.testsapp.ui.home.HomeFragment
+import com.oliverworks.testsapp.ui.notifications.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navigation : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
+        navigation = findViewById(R.id.nav_view)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.main_body, HomeFragment())
+            .commit()
+        navigation.setOnNavigationItemSelectedListener {
+            var fragment : Fragment? = null
+            fragment = when(it.itemId){
+                R.id.navigation_home -> HomeFragment()
+                R.id.navigation_dashboard -> SavedFragment()
+                else -> SettingsFragment()
+            }
+            supportFragmentManager.beginTransaction().replace(
+                R.id.main_body,
+                fragment
+            ).commit()
+            true
+        }
+    /*  val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navView.setupWithNavController(navController)*/
     }
 }
